dcfba05dcecc15c7266969f733fe2a50384d353c
Richard Ward <richard@richard.ward.name>
2020-12-06 12:29:27 +0000

    day 6.2, js, 162 chars

--- day6.js
+++ day6.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env -S bash -c '<day6-input.txt node $1' --
 console.log(eval(require('fs').readFileSync(0,'utf8').split('\n\n').map(g=>new Set(g.replace(/\s/g,'')).size).join("+")))
+console.log(eval(require('fs').readFileSync(0,'utf8').split('\n\n').map(g=>[...g.split('\n').reduce((l,r)=>[...l].filter(l=>r.indexOf(l)>=0))].length).join("+")))
