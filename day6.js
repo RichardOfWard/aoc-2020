c1e11c719858abf332b0d5ce56d5619cb06be9b5
Richard Ward <richard@richard.ward.name>
2020-12-06 12:46:11 +0000

    day 6 combined, 215 bytes excluding shebang

--- day6.js
+++ day6.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env -S bash -c '<day6-input.txt node $1' --
-console.log(eval(require('fs').readFileSync(0,'utf8').split('\n\n').map(g=>new Set(g.replace(/\s/g,'')).size).join("+")))
-console.log(eval(require('fs').readFileSync(0,'utf8').split('\n\n').map(g=>[...g.split('\n').reduce((l,r)=>[...l].filter(l=>r.indexOf(l)>=0))].length).join("+")))
+i=require('fs').readFileSync(0,'utf8')
+f=c=>console.log(eval(i.split('\n\n').map(c).join('+')))
+f(g=>new Set(g.replace(/\s/g,'')).size)
+f(g=>[...g.split('\n').reduce((l,r)=>[...l].filter(l=>r.indexOf(l)+1))].length)
