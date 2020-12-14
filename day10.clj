3692417cb00a0f8af8daae5c609a1e2dc3d6a60c
Richard Ward <richard@richard.ward.name>
2020-12-14 01:43:01 +0000

    10.2 clojure

--- day10.clj
+++ day10.clj
@@ -11,3 +11,28 @@ true; clojure $0 ; exit
      (apply *)
      println
      )
+
+(def count-possibilities #(0))
+
+(defn count-possibilities-raw [previous, [current & remaining]]
+  (if (> (count remaining) 0)
+    (if (> (- (first remaining) previous) 3)
+      (count-possibilities current remaining)
+      (+
+        (count-possibilities previous remaining)
+        (count-possibilities current remaining)
+        )
+      )
+    1
+    )
+  )
+(def count-possibilities (memoize count-possibilities-raw))
+
+(let [joltVals (->> (slurp "day10-input.txt")
+                    clojure.string/split-lines
+                    (map #(Integer/parseInt %))
+                    sort
+                    ((fn [x] (concat x [(+ 3 (apply max x))])))
+                    )]
+  (println (count-possibilities 0 joltVals))
+  )
