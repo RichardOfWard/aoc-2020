917441312b27279f53ae9ab386a9691630cc5a8d
Richard Ward <richard@richard.ward.name>
2020-12-16 12:14:46 +0000

    11.2 c#

--- day11.cs
+++ day11.cs
@@ -10,11 +10,33 @@ namespace day11
 
         static void Main(string[] args)
         {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
+        {
             var prevSeats = LoadInitial();
 
             var seats = new char[Rows, Cols];
 
             while (Tick(prevSeats, seats))
+            {
+                var tmp = prevSeats;
+                prevSeats = seats;
+                seats = tmp;
+            }
+
+            Console.WriteLine(Count(seats));
+        }
+
+        private static void Part2()
+        {
+            var prevSeats = LoadInitial();
+
+            var seats = new char[Rows, Cols];
+
+            while (Tick2(prevSeats, seats))
             {
                 var tmp = prevSeats;
                 prevSeats = seats;
@@ -42,6 +64,74 @@ namespace day11
                             if (prevSeats[nRow, nCol] == '#')
                             {
                                 occupied += 1;
+                            }
+                        }
+                    }
+
+                    switch (prevSeat)
+                    {
+                        case 'L':
+                        {
+                            if (occupied == 0)
+                            {
+                                seats[row, col] = '#';
+                                changed = true;
+                            }
+
+                            break;
+                        }
+                        case '#':
+                        {
+                            if (occupied > 4)
+                            {
+                                seats[row, col] = 'L';
+                                changed = true;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return changed;
+        }
+
+        private static bool Tick2(char[,] prevSeats, char[,] seats)
+        {
+            var changed = false;
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var col = 0; col < Cols; col++)
+                {
+                    var prevSeat = prevSeats[row, col];
+
+                    seats[row, col] = prevSeat;
+                    var occupied = 0;
+                    for (var dr = -1; dr <= 1; dr++)
+                    {
+                        for (var dc = -1; dc <= 1; dc++)
+                        {
+                            if (dr == 0 && dc == 0)
+                            {
+                                continue;
+                            }
+
+                            for (var n = 1;
+                                row + n * dr < Rows && row + n * dr >= 0 && col + n * dc < Cols && col + n * dc >= 0;
+                                n++)
+                            {
+                                var prevSeatSeen = prevSeats[row + n * dr, col + n * dc];
+                                if (prevSeatSeen == 'L')
+                                {
+                                    break;
+                                }
+
+                                if (prevSeatSeen == '#')
+                                {
+                                    occupied += 1;
+                                    break;
+                                }
                             }
                         }
                     }
