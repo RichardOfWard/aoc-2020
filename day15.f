6adbd434169178f681308b3896c08ed5714802ba
Richard Ward <richard@richard.ward.name>
2022-10-12 11:54:37 +0100

    some uncommited bits

---
+++ day15.f
@@ -0,0 +1,8 @@
+      program addNumbers
+              implicit none
+
+              integer, dimension(2020) :: numbers
+
+              numbers(1) = 123;
+              print *, numbers(1)
+      end program addNumbers
